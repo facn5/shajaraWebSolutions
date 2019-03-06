685bc98d1366be0f5fc4eb8f58a7378172f568fd
karam1ashqar <karam.ashqar01@gmail.com>
2019-03-06 10:55:10 +0200

    s

--- script.js
+++ script.js
@@ -6,3 +6,25 @@ function biograph(e) {
 		e.className = 'about__slot';
 	}
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+
+
+window.onscroll = function() {myFunction()};
+
+
+var navbar = document.getElementById("team-members");
+var navbar1 = document.getElementById("navbar");
+
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+		navbar1.style.display = "block"
+    navbar1.classList.add("sticky")
+  } else {
+    navbar1.classList.remove("sticky");
+		navbar1.style.display = "none"
+  }
+}
+})
