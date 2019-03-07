98454518bd8c2b094baa06020c53131fa0ca8324
karam1ashqar <karam.ashqar01@gmail.com>
2019-03-07 15:39:42 +0200

    phone number format fixes

--- script.js
+++ script.js
@@ -10,15 +10,16 @@ function biograph(element) {
 
 document.addEventListener('DOMContentLoaded', function() {
 	var phoneID = document.getElementById('phoneNumber')
+  var phoneLabelID = document.getElementById('phoneNumberLabel')
 
 	phoneID.addEventListener('focusin', function() {
 		phoneID.placeholder = "050 555 5555"
-		phoneID.value = "What is your phone number? (555 555 5555)"
+		phoneLabelID.innerContent = "What is your phone number? (555 555 5555)"
 	  }, true);
 
 		phoneID.addEventListener('focusout', function() {
 			phoneID.placeholder = ""
-			phoneID.value = "What is your phone number?"
+			phoneLabelID.innerContent = "What is your phone number?"
 
 		}, false);
 
